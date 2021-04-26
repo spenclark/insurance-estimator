c521e4d86be3242ac20536b65f0573206ecb5b5d
Spencer <spenclark@hotmail.com>
2021-04-25 21:18:21 -0700

    db

--- app_backend/data/migrations/20210425220118_002_CreateUser.js
+++ app_backend/data/migrations/20210425220118_002_CreateUser.js
@@ -1,3 +1,13 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("user", (tbl) => {
+    tbl.uuid("id");
+    tbl.string("email", 64);
+    tbl.date("DOB");
+    tbl.string("nationality", 64);
+    tbl.jsonb("perks");
+    tbl.integer("coverage");
+    tbl.float("final_rate"); // this float is what gets passback to frontend
+  });
+};
 
 exports.down = function (knex) {};
