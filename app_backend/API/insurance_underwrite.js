4840e016bc6d9a91df5e38c64dd16b01cf719dd3
Spencer <spenclark@hotmail.com>
2021-04-26 17:18:57 -0700

    formatDate test

--- app_backend/API/insurance_underwrite.js
+++ app_backend/API/insurance_underwrite.js
@@ -1,17 +1,16 @@
-// We want to evalute both the policy model and the user data and return an aprox. monthly cost
-
 // Formula Sketch
-// (Coverage(int) / (policy_length(yrs) * 12))(risk_rate(float)(age_risk^inverse(risk_rate)) + sum of each in perk_array.monthly_cost = estimated_monthly_subscription
 // linear time complexity bc iterative loop on perk_array
 
-function estimate_policy(coverage, policy_length, risk_rate, age, perk_array) {
-  // this increments each monthly_cost attr in the perk array giving us a total
-  //   var perk_sum = 0;
-  //   perk_array.forEach((e) => {
-  //     perk_sum += e.monthly_cost;
-  //   });
-  //   this assumes that for each year user the chance of needing to payout policy increases by x1.075 over the course of the policy term
-  //   return Math.round((coverage / (policy_length * 12)) * risk_rate + perk_sum);
+function estimate_policy(coverage, policy_length, risk_rate, DOB, perk_array) {
+  // increment each monthly_cost attr in the perk_array into a varible
+  var perk_sum = 0;
+  perk_array.forEach((e) => {
+    perk_sum += e.monthly_cost;
+  });
+
+  //   this assumes that for each year the chance of needing to payout policy by x1.075 + 0.01 for each year user is over 18 the course of the policy term if
+
+  return Math.round((coverage / (policy_length * 12)) * risk_rate + perk_sum);
 }
 
 console.log(
