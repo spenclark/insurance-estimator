d7bd250fd6ff6b30384f27340cac00a4ea281ee9
Spencer <spenclark@hotmail.com>
2021-05-21 15:42:17 -0700

    perk models

--- app_backend/API/models/policy.model.js
+++ app_backend/API/models/policy.model.js
@@ -3,6 +3,7 @@ const db = require("../../data/dbConfig");
 module.exports = {
   findPolicy,
   addPolicy,
+  returnPerks,
 };
 
 function findPolicy(id) {
@@ -12,3 +13,9 @@ function findPolicy(id) {
 function addPolicy(input) {
   return ([id] = await db("policy_model").insert(input, "id"));
 }
+
+function returnPerks(id) {
+  return db("perks").where(id, "=", "policy_id");
+}
+//  use innerjoin method to find where policy id are equal on perk and user table
+// function findSelectedPerk() {}
